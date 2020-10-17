011b2b14f4752f7dfd56243e6d087eb8a8ab6556
evertonab28 <evertonab2005@gmail.com>
2020-10-16 23:47:47 -0400

    adding-views

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -1,27 +1,54 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import OrphanageDetails from './pages/OrphanageDetails';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import OrphanageDetails from "./pages/OrphanageDetails";
 
 const { Navigator, Screen } = createStackNavigator();
 
-import OrphanagesMap from './pages/OrphanagesMap';
+import OrphanagesMap from "./pages/OrphanagesMap";
 
-export default function Routes(){
-    return (
-        <NavigationContainer>
-            {/* desabilita o header globalmente */}
-            <Navigator screenOptions={{ headerShown: false }}>
-                <Screen 
-                name="OrphanagesMap" 
-                component={OrphanagesMap} 
-                />
+import SelectMapPosition from "./pages/CreateOrphanage/SelectMapPosition";
+import OrphanageData from "./pages/CreateOrphanage/OrphanageData";
 
-                <Screen 
-                name="OrphanageDetails" 
-                component={OrphanageDetails} 
-                /> 
-            </Navigator>
-        </NavigationContainer>
-    );
+import Header from "./components/Header";
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      {/* desabilita o header globalmente */}
+      <Navigator screenOptions={{ headerShown: false, cardStyle: {backgroundColor: '#f2f3f5'} }}>
+        <Screen 
+        name="OrphanagesMap" 
+        component={OrphanagesMap} 
+        />
+
+        <Screen 
+        name="OrphanageDetails" 
+        component={OrphanageDetails} 
+        options={{
+          headerShown: true,
+          header: () => <Header showCancel={false} title="Orfanato"/>
+        }}
+        />
+
+        <Screen 
+        name="SelectMapPosition" 
+        component={SelectMapPosition} 
+        options={{
+          headerShown: true,
+          header: () => <Header title="Selecione no mapa"/>
+        }}
+        />
+
+        <Screen 
+        name="OrphanageData" 
+        component={OrphanageData} 
+        options={{
+          headerShown: true,
+          header: () => <Header title="Informe os dados"/>
+        }}
+        />
+      </Navigator>
+    </NavigationContainer>
+  );
 }
